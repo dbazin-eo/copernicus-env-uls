c0df392f7b88cee61323060a71cd812dd83e2f63
copernicusmarine <51791874+copernicusmarine@users.noreply.github.com>
2019-11-03 20:47:15 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,8 @@
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
-install.packages("tidyverse")
+install.packages("ncdf.tools")
+install.packages("ncdf4",repos = "http://cirrus.ucsd.edu/~pierce/ncdf ")
 install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
